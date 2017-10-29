47f1908355c963116544aba2e7ca22745932f0bd
Andreas Zimmerer <andreas.zimmerer@tum.de>
2017-10-29 21:37:40 +0100

    switched to piston_window

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 //! An implementation of the popular 2048 game in Rust with Piston Game Engine
 
 extern crate piston;
-extern crate glutin_window;
+extern crate piston_window;
 extern crate graphics;
 extern crate opengl_graphics;
 
 use piston::window::WindowSettings;
 use piston::event_loop::{Events, EventSettings, EventLoop};
 use piston::input::RenderEvent;
-use glutin_window::GlutinWindow;
+use piston_window::*;
 use opengl_graphics::{OpenGL, GlGraphics};
 
 pub use gameboard::Gameboard;
@@ -21,15 +21,13 @@ mod gameboard_controller;
 mod gameboard_view;
 
 fn main() {
-    let opengl = OpenGL::V3_2;
-    let settings = WindowSettings::new("2048", [512; 2])
-        .opengl(opengl)
-        .exit_on_esc(true);
-    let mut window: GlutinWindow = settings.build()
+    let mut window: PistonWindow = WindowSettings::new("2048", (512,512))
+        .exit_on_esc(true)
+        .build()
         .expect("Could not create window");
 
     let mut events = Events::new(EventSettings::new().lazy(true));
-    let mut gl = GlGraphics::new(opengl);
+    //let mut gl = GlGraphics::new(opengl);
 
     let gameboard = Gameboard::new();
     let mut gameboard_controller = GameboardController::new(gameboard);
@@ -39,7 +37,7 @@ fn main() {
     while let Some(e) = events.next(&mut window) {
         gameboard_controller.event(&e);
         if let Some(args) = e.render_args() {
-            gl.draw(args.viewport(), |c, g| {
+            window.draw_2d(&e, |c, g| {
                 use graphics::{clear};
 
                 clear([1.0; 4], g);
@@ -47,5 +45,4 @@ fn main() {
             });
         }
     }
-    println!("{}", settings.get_exit_on_esc());
 }
